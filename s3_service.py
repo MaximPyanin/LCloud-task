b2cafa04164247c6b37349134cd410ad735e9bb0
MaximPyanin <maximpyanin@icloud.com>
2024-09-30 12:44:41 +0300

    added get_object func to list all files from a bucket

--- s3_service.py
+++ s3_service.py
@@ -10,3 +10,13 @@ class S3Service:
             aws_access_key_id=self.config.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=self.config.AWS_SECRET_ACCESS_KEY,
         )
+
+    def get_objects(self):
+        response = self._client.list_objects_v2(
+            Bucket=self.config.BUCKET_NAME, Prefix=self.config.DIRECTORY
+        )
+        if "Contents" in response:
+            for item in response["Contents"]:
+                print(item["Key"])
+        else:
+            print("No files found")
